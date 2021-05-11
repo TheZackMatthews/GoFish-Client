2f6451529030df6e72f7aca1645ac3764170f93f
Zack <TheZackMatthews>
2021-05-10 17:48:56 -0700

    Feat: Added eslint and action newFieldVisit

--- redux/actions/surveyActions.js
+++ redux/actions/surveyActions.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
+import axios from "axios"
 import { NEW_FIELD_VISIT, UPDATE_ENTRY, SAVE_SURVEY } from './actionTypes'
- 
+
 export const initializeSurvey = (user) => dispatch => {
   // here, we could add the initial info:
   // location info from map for starting location
@@ -12,3 +14,22 @@ export const initializeSurvey = (user) => dispatch => {
     }
   })
 }
+
+// creek_name: string, team_lead: string, team_members: string[]
+export const initializeFieldVisit = (creekName, teamLead, teamMembers) => (dispatch) => {
+  const volunteers = {
+    creekName,
+    teamLead,
+    teamMembers,
+  };
+  axios.post('http://localhost:3001', volunteers)
+    .then((response) => {
+      console.log(response);
+      return dispatch({
+        type: NEW_FIELD_VISIT,
+        payload: {
+          response,
+        },
+      });
+    });
+};
