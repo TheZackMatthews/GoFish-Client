40aa2ba49c0c0d9afa01c9c79d5d62c8508de9c3
Kimberly Innes <kimberly.innes@gmail.com>
2021-05-12 17:48:54 -0400

    fixed merge issues

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { name as appName } from "./app.json";
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  AppRegistry, StyleSheet, View,
+} from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { name as appName } from './app.json';
 import {
   Fish1,
   Camera,
   SignIn,
   SignUp,
-  EditUserInfo,
   ProjectMap,
   ChinookProfile,
   ReferenceInfo,
@@ -22,6 +23,7 @@ import {
   DayStart,
   FishDead1,
   FishDead2,
+  EditUserInfo,
 } from './screens/index';
 import { useStore } from './redux/store';
 import AuthProvider from './auth';
@@ -31,13 +33,16 @@ const theme = {
   ...DefaultTheme,
 };
 
-import { useStore } from "./redux/store";
-import AuthProvider from "./auth";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
 
 export default function App() {
   const store = useStore({});
-
-  console.log("App is working");
+  console.log('App is working');
   const Stack = createStackNavigator();
 
   return (
@@ -48,17 +53,12 @@ export default function App() {
             <NavigationContainer>
               <Stack.Navigator
                 screenOptions={{ headerShown: false }}
-                initialRouteName="FishAlive1"
+                initialRouteName="ProjectMap"
               >
-                <Stack.Screen name="Fish1" component={Fish1} />
-                <Stack.Screen name="ProjectMap" component={ProjectMap} />
                 <Stack.Screen name="SignIn" component={SignIn} />
                 <Stack.Screen name="SignUp" component={SignUp} />
-                <Stack.Screen
-                  name="ProjectProfile"
-                  component={ProjectProfile}
-                />
                 <Stack.Screen name="Profile" component={UserProfile} />
+                <Stack.Screen name="EditUserInfo" component={EditUserInfo} />
                 {/* Map Pages */}
                 <Stack.Screen name="ProjectMap" component={ProjectMap} />
                 <Stack.Screen name="UserMap" component={UserMap} />
@@ -86,16 +86,5 @@ export default function App() {
     </Provider>
   );
 }
-
-const theme = {
-  ...DefaultTheme,
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-  },
-});
 
 AppRegistry.registerComponent(appName, () => Main);
