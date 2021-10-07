07153e7b1ed769e3bc63974a3d2c816fcf79ba50
Zack <zackary.matthews@yahoo.com>
2021-10-06 21:52:17 -0700

    Fix: Cleanup

--- auth/firebaseConfig.js
+++ auth/firebaseConfig.js
@@ -1,24 +1,23 @@
-// import {
-//   // eslint-disable-next-line camelcase
-//   FB_apiKey, FB_authDomain, FB_projectId, FB_storageBucket, FB_messagingSenderId, FB_appId,
-// // eslint-disable-next-line import/no-unresolved
-// } from '@env';
+/* eslint-disable camelcase */
+import {
+  FB_apiKey,
+  FB_authDomain,
+  FB_projectId,
+  FB_storageBucket,
+  FB_messagingSenderId,
+  FB_appId,
+  FB_measurementId,
+// eslint-disable-next-line import/no-unresolved
+} from '@env';
 
-// const FB_CONFIG = {
-//   apiKey: FB_apiKey,
-//   authDomain: FB_authDomain,
-//   projectId: FB_projectId,
-//   storageBucket: FB_storageBucket,
-//   messegingSenderId: FB_messagingSenderId,
-//   appId: FB_appId,
-// };
 const FB_CONFIG = {
-  apiKey: 'AIzaSyA-BlslvHs0KyNZcMmde-aO6TCwSCSJR3w',
-  authDomain: 'gofish-auth-e24fe.firebaseapp.com',
-  projectId: 'gofish-auth-e24fe',
-  storageBucket: 'gofish-auth-e24fe.appspot.com',
-  messagingSenderId: '404257520865',
-  appId: '1:404257520865:web:89a738ff301ae2789c8ba0',
-  measurementId: 'G-03V7C098EE',
+  apiKey: FB_apiKey,
+  authDomain: FB_authDomain,
+  projectId: FB_projectId,
+  storageBucket: FB_storageBucket,
+  messegingSenderId: FB_messagingSenderId,
+  appId: FB_appId,
+  measurementId: FB_measurementId,
 };
+
 export default FB_CONFIG;
